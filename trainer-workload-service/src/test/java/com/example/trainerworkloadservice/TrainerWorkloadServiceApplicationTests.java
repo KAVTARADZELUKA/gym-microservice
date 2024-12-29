73626e4d7f77b01e9d919dee313420539ede26f6
Luka Kavtaradze <lukakavtaradze2001@gmail.com>
2024-12-29 22:43:35 +0400

    remove tests depends

--- trainer-workload-service/src/test/java/com/example/trainerworkloadservice/TrainerWorkloadServiceApplicationTests.java
+++ trainer-workload-service/src/test/java/com/example/trainerworkloadservice/TrainerWorkloadServiceApplicationTests.java
@@ -1,10 +1,28 @@
 package com.example.trainerworkloadservice;
 
+import com.example.trainerworkloadservice.mysql.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootTest
+@SpringBootTest(
+        properties = {
+                "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration," +
+                        "org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration," +
+                        "org.springframework.cloud.netflix.eureka.EurekaClientAutoConfiguration"
+        }
+)
 class TrainerWorkloadServiceApplicationTests {
+
+    @Configuration
+    static class MockConfig {
+        @Bean
+        UserRepository userRepository() {
+            return Mockito.mock(UserRepository.class);
+        }
+    }
 
     @Test
     void contextLoads() {
